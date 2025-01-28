4f46be2078f3ee41fe23932b2aa5a40c7a5d7d3d
hamza-nafasat <hamzanafasat4155@gmail.com>
2025-01-28 19:24:15 +0500

    add cookies option

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from routes.user_routes import router as user_router
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+app.include_router(user_router)
