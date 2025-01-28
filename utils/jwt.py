3267f6ac769081c5ec3134e3f1265480459c511b
hamza-nafasat <hamzanafasat4155@gmail.com>
2025-01-28 19:23:27 +0500

    add jwt functions

---
+++ utils/jwt.py
@@ -0,0 +1,37 @@
+from datetime import datetime, timedelta
+from typing import Any, Optional
+from jose import JWTError, jwt
+from configs.envConf import getEnv
+
+ALGORITHM = "HS256"
+ACCESS_SECRET = getEnv("ACCESS_TOKEN_SECRET_KEY")
+REFRESH_SECRET = getEnv("REFRESH_TOKEN_SECRET_KEY")
+ACCESS_TIME = int(getEnv("ACCESS_TOKEN_EXPIRE_SECONDS"))
+REFRESH_TIME = int(getEnv("REFRESH_TOKEN_EXPIRE_SECONDS"))
+
+
+# Function to create an access token
+def create_access_token(data: dict, expires_minutes: Optional[int] = None) -> str:
+    expires_minutes = expires_minutes or ACCESS_TIME
+    expire = datetime.utcnow() + timedelta(seconds=expires_minutes)
+    to_encode = data.copy()
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, ACCESS_SECRET, algorithm=ALGORITHM)
+
+
+# Function to create a refresh token
+def create_refresh_token(data: dict, expires_minutes: Optional[int] = None) -> str:
+    expires_minutes = expires_minutes or REFRESH_TIME
+    expire = datetime.utcnow() + timedelta(seconds=expires_minutes)
+    to_encode = data.copy()
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, REFRESH_SECRET, algorithm=ALGORITHM)
+
+
+# Function to decode the JWT token
+def decode_token(token: str, secret_key: str) -> Optional[dict]:
+    try:
+        payload = jwt.decode(token, secret_key, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
