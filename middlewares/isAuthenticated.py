ef1722606fc32d0fdc6cf7ea32ddaee856863f94
hamza-nafasat <hamzanafasat4155@gmail.com>
2025-01-29 16:09:20 +0500

    make is authenticated middleware

---
+++ middlewares/isAuthenticated.py
@@ -0,0 +1,41 @@
+from fastapi import Request, HTTPException, Response
+from configs.envConf import getEnv
+from utils.jwt import create_access_token, decode_token
+from configs.database import User
+from configs.constants import accessTokenOptions
+from bson import ObjectId
+
+
+async def isAuthenticated(request: Request, response: Response):
+    try:
+        # Get access token from cookies   Decode access token and fetch user
+        accessToken = request.cookies.get(getEnv("ACCESS_TOKEN_NAME"))
+        if accessToken:
+            accessPayload = decode_token(accessToken, getEnv("ACCESS_TOKEN_SECRET"))
+            if not accessPayload:
+                raise HTTPException(status_code=401, detail="Access token not decoded")
+            user = await User.find_one({"_id": ObjectId(accessPayload["_id"])})
+            if not user:
+                raise HTTPException(
+                    status_code=401, detail="User not found from access token"
+                )
+            return user
+        # If access token is missing, check for refresh token   Decode refresh token and fetch user
+        refreshToken = request.cookies.get(getEnv("REFRESH_TOKEN_NAME"))
+        if not refreshToken:
+            raise HTTPException(status_code=401, detail="Refresh token expired")
+        refreshPayload = decode_token(refreshToken, getEnv("REFRESH_TOKEN_SECRET"))
+        if not refreshPayload:
+            raise HTTPException(status_code=401, detail="Refresh token not decoded")
+        user = await User.find_one({"_id": ObjectId(refreshPayload["_id"])})
+        if not user:
+            raise HTTPException(
+                status_code=401, detail="User not found from refresh token"
+            )
+        # Generate new access token
+        accessToken = create_access_token({"_id": str(user["_id"])})
+        response.set_cookie(value=accessToken, **accessTokenOptions)
+        return user
+    except Exception as e:
+        print("Error in isAuthenticated middleware:", str(e))
+        raise HTTPException(status_code=401, detail="Please Login Again")
