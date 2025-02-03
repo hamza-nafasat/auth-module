91c45a76575393747acc8065f3fa0fe79da907f8
hamza-nafasat <hamzanafasat4155@gmail.com>
2025-02-03 13:21:49 +0500

    add type safety in token

--- utils/sendTokens.py
+++ utils/sendTokens.py
@@ -1,6 +1,6 @@
 from fastapi.responses import JSONResponse
 from utils.features import sendError
-from utils.jwt import create_access_token, create_refresh_token
+from utils.jwt import create_jwt_token
 from configs.envConf import config, getEnv
 from configs.constants import accessTokenOptions, refreshTokenOptions
 
@@ -10,8 +10,16 @@ def sendTokens(statusCode: int, message: str, data: dict):
         if not data["_id"]:
             return sendError(500, "User creation failed")
         # create access and refresh token
-        accessToken = create_access_token({"_id": data["_id"]})
-        refreshToken = create_refresh_token({"_id": data["_id"]})
+        accessToken = create_jwt_token(
+            {"_id": data["_id"]},
+            getEnv("ACCESS_TOKEN_SECRET"),
+            int(getEnv("ACCESS_TOKEN_EXPIRE_SECONDS")),
+        )
+        refreshToken = create_jwt_token(
+            {"email": data["email"]},
+            getEnv("REFRESH_TOKEN_SECRET"),
+            int(getEnv("REFRESH_TOKEN_EXPIRE_SECONDS")),
+        )
         # send access token and refresh token in user cookies
         if not accessToken or not refreshToken:
             return sendError(500, "Error in Authentication, login again")
